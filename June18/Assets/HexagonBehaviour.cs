21cad8f0d5dc030f3b36cb85e1c627c00abde071
Unjson <jhaenjes@web.de>
2018-06-08 20:06:33 +0000

    Unity project Added

---
+++ June18/Assets/HexagonBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonBehaviour : MonoBehaviour {
+
+
+    public GameObject aboveLeft;
+	public GameObject aboveRight;
+	public GameObject left;
+	public GameObject right;
+	public GameObject belowLeft;
+	public GameObject belowRight;
+
+	public string inhabitant;
+
+    // Use this for initialization
+    void Start () {
+	
+		inhabitant = null;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+
+}
---
+++ June18/Assets/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject positon;
+
+	private HexagonBehaviour homeHex;
+	private HexagonBehaviour destHex;
+
+	// Use this for initialization
+	void Start () {
+		
+ 		homeHex = positon.gameObject.GetComponent<HexagonBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//INPUT MANAGER
+
+		if(Input.GetButtonDown("UpLeft")){
+
+			destHex = homeHex.aboveLeft.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.aboveLeft.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+
+		}
+
+		if(Input.GetButtonDown("UpRight")){
+
+			destHex = homeHex.aboveRight.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.aboveRight.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+		}
+
+		if(Input.GetButtonDown("Left")){
+
+			destHex = homeHex.left.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.left.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+		}
+
+		if(Input.GetButtonDown("Right")){
+
+			destHex = homeHex.right.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.right.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+		}
+
+		if(Input.GetButtonDown("DownLeft")){
+
+			destHex = homeHex.belowLeft.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.belowLeft.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+		}
+
+		if(Input.GetButtonDown("DownRight")){
+
+			destHex = homeHex.belowRight.GetComponent<HexagonBehaviour>();
+
+			if (destHex.inhabitant == null) {
+
+				//Set new Home
+				this.transform.position = homeHex.belowRight.transform.position;
+				homeHex = destHex;
+				positon = destHex.gameObject;
+
+			}
+		}
+				
+
+	}
+}
